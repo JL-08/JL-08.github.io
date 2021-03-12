e7e421a732b2a50c0c9132bf8fd8c7aa5966de29
JL-08 <janellelacsamana@gmail.com>
2021-03-12 17:58:31 +0800

    section(preloader): add page preloader

---
+++ preloader.js
@@ -0,0 +1,7 @@
+window.onload = function () {
+  var main = document.querySelector('main');
+  var preloader = document.querySelector('.preloader');
+
+  main.style.display = 'initial';
+  preloader.style.display = 'none';
+};
